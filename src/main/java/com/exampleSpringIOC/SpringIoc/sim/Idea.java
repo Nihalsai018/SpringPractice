671416b8957d16af8978bb43723d7ac9c2ec4a1c
Nihalsai018 <nihalkanna018@gmail.com>
2024-01-17 13:42:02 +0530

    added slot method

--- src/main/java/com/exampleSpringIOC/SpringIoc/sim/Idea.java
+++ src/main/java/com/exampleSpringIOC/SpringIoc/sim/Idea.java
@@ -2,7 +2,7 @@ package com.exampleSpringIOC.SpringIoc.sim;
 
 public class Idea implements Sim {
 
-  
+  //slot();
     @Override
     public void simslot() {
         System.out.println("inserted idea sim");
