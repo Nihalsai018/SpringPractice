88ab78d17d63dca19b18e7150756e4b097c805c5
Chaitanya Nampalli <chaitanyamurali.nampalli@serole.com>
2024-01-17 13:33:34 +0530

    added cmnt

--- src/main/java/com/exampleSpringIOC/SpringIoc/sim/Idea.java
+++ src/main/java/com/exampleSpringIOC/SpringIoc/sim/Idea.java
@@ -2,6 +2,9 @@ package com.exampleSpringIOC.SpringIoc.sim;
 
 public class Idea implements Sim {
 
+    /*
+    simSlot
+     */
     @Override
     public void simslot() {
         System.out.println("inserted idea sim");
