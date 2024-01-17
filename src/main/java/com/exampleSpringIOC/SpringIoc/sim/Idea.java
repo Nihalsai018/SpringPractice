d741435d4dcf38c1644090f8cebdd68fb1bd3232
GADE NIHAL SAI <119513449+Nihalsai018@users.noreply.github.com>
2024-01-17 13:38:59 +0530

    removed sim method

--- src/main/java/com/exampleSpringIOC/SpringIoc/sim/Idea.java
+++ src/main/java/com/exampleSpringIOC/SpringIoc/sim/Idea.java
@@ -2,9 +2,7 @@ package com.exampleSpringIOC.SpringIoc.sim;
 
 public class Idea implements Sim {
 
-    /*
-    simSlot
-     */
+  
     @Override
     public void simslot() {
         System.out.println("inserted idea sim");
